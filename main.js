5d05abab375a3bab8de8c176fdbf0964ff671f38
okapicapybara88 <81312147+okapicapybara88@users.noreply.github.com>
2021-07-01 09:18:42 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+var images = ["https://i.postimg.cc/MKdhy06Z/family.jpg","mom.jpg", "dad.jpg" , "ana.jpg", "atticus.JPG", "addy.jpg","family.JPG"];
+var names = ["Family Book","Jasmine Taylor", "Aaron Taylor", "Anastasia Taylor", "Atticus Taylor", "Adisenne Taylor"  ,"My family"];
+var i = 0;
+function update()
+{
+    i++;
+    var numbers_of_family_member_in_array = 6
+    if(i > numbers_of_family_member_in_array )
+    {
+        i = 0;
+    }
+    var updatedImage = images[i];
+    var updatedName  = names[i];
+    document.getElementById("family_member_image").src = updatedImage;
+    document.getElementById("family_member_name").innerHTML = updatedName;
+}
